3414c3d31d8db5be728e8203f37089c9afd5e7c5
YiCheng <dekknm0971@gmail.com>
2020-04-24 22:44:08 +0800

    add template

---
+++ admin/backstageTemplates/content.php
@@ -0,0 +1,12 @@
+<?php require_once('header.php');
+      require_once('leftSideBar.php');
+      require_once('rightContainer.php');
+?>
+
+<div style="width:400px; height:400px; background:#000;">
+123123</div>
+
+<?php
+  require_once('footer.php');
+?>
+
---
+++ admin/backstageTemplates/rightContainer.php
@@ -0,0 +1,15 @@
+<div class="wrapper">
+  <!-- Content -->
+  <div id="content">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="#"><i class="zmdi zmdi-notifications text-danger"></i>
+              </a>
+            </li>
+            <li><a href="#">Admin</a></li>
+          </ul>
+        </div>
+      </nav>
+  </div>
