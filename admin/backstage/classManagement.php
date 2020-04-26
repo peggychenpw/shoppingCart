0ea133d7db33128107b9ac478fc82aa42af93dda
YiCheng <dekknm0971@gmail.com>
2020-04-27 00:26:48 +0800

    deleteClass

--- admin/backstage/classManagement.php
+++ admin/backstage/classManagement.php
@@ -40,23 +40,24 @@ $totalClassCatogories = $pdo->query($sqlTotalClassCatogories)->fetch(PDO::FETCH_
 //若有建立商品種類，則顯示商品清單
 if ($totalClassCatogories > 0) {
 ?>
-  <table class="table table-striped table-gray text-center">
-    <thead class="thead-dark">
-      <tr>
-        <th class="border">勾選</th>
-        <th class="border">課程名稱</th>
-        <th class="border">課程價格</th>
-        <th class="border">課程類別</th>
-        <th class="border">上線人數</th>
-        <th class="border">上課日期</th>
-        <th class="border">上線時間</th>
-        <th class="border">功能</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      //SQL 敘述
-      $sql = "SELECT `class`.`id`,`class`.`classId`, `class`.`className`, `class`.`classPrice`, `classcategory`.`classCategoryName`, 
+  <form method="POST" enctype="multipart/form-data" action="deleteClass.php">
+    <table class="table table-striped table-gray text-center">
+      <thead class="thead-dark">
+        <tr>
+          <th class="border">勾選</th>
+          <th class="border">課程名稱</th>
+          <th class="border">課程價格</th>
+          <th class="border">課程類別</th>
+          <th class="border">上線人數</th>
+          <th class="border">上課日期</th>
+          <th class="border">上線時間</th>
+          <th class="border">功能</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        //SQL 敘述
+        $sql = "SELECT `class`.`id`,`class`.`classId`, `class`.`className`, `class`.`classPrice`, `classcategory`.`classCategoryName`, 
                         `class`.`classPeopleLimit`, `class`.`classDate`, `class`.`classTime`, `class`.`created_at`,
                         `class`.`updated_at`
                 FROM `class` INNER JOIN `classcategory`
@@ -64,62 +65,63 @@ if ($totalClassCatogories > 0) {
                 ORDER BY `class`.`id` ASC 
                 LIMIT ?, ? ";
 
-      //設定繫結值
-      $arrParam = [($page - 1) * $numPerPage, $numPerPage];
+        //設定繫結值
+        $arrParam = [($page - 1) * $numPerPage, $numPerPage];
 
-      //查詢分頁後的商品資料
-      $stmt = $pdo->prepare($sql);
-      $stmt->execute($arrParam);
+        //查詢分頁後的商品資料
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($arrParam);
 
-      //若數量大於 0，則列出商品
-      if ($stmt->rowCount() > 0) {
-        $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        for ($i = 0; $i < count($arr); $i++) {
-      ?>
+        //若數量大於 0，則列出商品
+        if ($stmt->rowCount() > 0) {
+          $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          for ($i = 0; $i < count($arr); $i++) {
+        ?>
+            <tr>
+              <td class="border classTd">
+                <input type="hidden" name="" value="<?php echo count($arr) ?>">
+                <input id='test<?php echo $i ?>' type="checkbox" name="chk[]" value="<?php echo $arr[$i]['id']; ?>" />
+              </td>
+              <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['className']; ?></td>
+              <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classPrice']; ?></td>
+              <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classCategoryName']; ?></td>
+              <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classPeopleLimit']; ?></td>
+              <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classDate']; ?></td>
+              <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classTime']; ?></td>
+              <td class="border">
+                <a class="_btn" href="./classInfo.php?id=<?php echo $arr[$i]['id'] ?>">詳細資訊</a> |
+                <a href="./comments.php?itemId=<?php echo $arr[$i]['itemId']; ?>">回覆評論</a>
+              </td>
+            </tr>
+          <?php
+          }
+        } else {
+          ?>
           <tr>
-            <td class="border classTd">
-              <input type="hidden" name="" value="<?php echo count($arr) ?>">
-              <input id='test<?php echo $i ?>' type="checkbox" name="chk[]" value="<?php echo $arr[$i]['id']; ?>" />
-            </td>
-            <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['className']; ?></td>
-            <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classPrice']; ?></td>
-            <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classCategoryName']; ?></td>
-            <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classPeopleLimit']; ?></td>
-            <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classDate']; ?></td>
-            <td class="border input<?php echo $i ?>"><?php echo $arr[$i]['classTime']; ?></td>
-            <td class="border">
-              <a class="_btn" href="./classInfo.php?id=<?php echo $arr[$i]['id'] ?>">詳細資訊</a> |
-              <a href="./comments.php?itemId=<?php echo $arr[$i]['itemId']; ?>">回覆評論</a>
-            </td>
+            <td class="border" colspan="8">沒有資料</td>
           </tr>
         <?php
         }
-      } else {
         ?>
+      </tbody>
+      <tfoot>
         <tr>
-          <td class="border" colspan="8">沒有資料</td>
+          <td class="border" colspan="8">
+            <?php for ($i = 1; $i <= $totalPages; $i++) { ?>
+              <a href="?page=<?= $i ?>"><?= $i ?></a>
+            <?php } ?>
+          </td>
         </tr>
-      <?php
-      }
-      ?>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td class="border" colspan="8">
-          <?php for ($i = 1; $i <= $totalPages; $i++) { ?>
-            <a href="?page=<?= $i ?>"><?= $i ?></a>
-          <?php } ?>
-        </td>
-      </tr>
 
-      <?php if ($total > 0) { ?>
-        <tr>
-          <td class="border" colspan="8"><input type="submit" name="smb" value="刪除"></td>
-        </tr>
-      <?php } ?>
+        <?php if ($total > 0) { ?>
+          <tr>
+            <td class="border" colspan="8"><input type="submit" name="smb" value="刪除"></td>
+          </tr>
+        <?php } ?>
 
-    </tfoot>
-  </table>
+      </tfoot>
+    </table>
+  </form>
 <?php
 } else {
   //引入尚未建立商品種類的文字描述
---
+++ admin/backstage/deleteClass.php
@@ -0,0 +1,20 @@
+<?php
+require_once('./checkAdmin.php'); //引入登入判斷
+require_once('./db.inc.php'); //引用資料庫連線
+
+$count = 0;
+for ($i = 0; $i < count($_POST['chk']); $i++) {
+  //加入繫結陣列
+  $arrParam = [
+    $_POST['chk'][$i]
+  ];
+  $sql = "DELETE FROM `class` WHERE `id` = ? ";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute($arrParam);
+
+  //累計每次刪除的次數
+  $count += $stmt->rowCount();
+}
+if ($count > 0) {
+  header("Refresh: 0;url=./classManagement.php");
+}
