7aef635efd693466ae505ea282f28d9ca5bba652
peggy <ya5471ya@gmail.com>
2020-04-27 18:10:18 +0800

    !!

--- admin/Insense/backStage/deleteCheck.php
+++ admin/Insense/backStage/deleteCheck.php
@@ -1,6 +1,7 @@
+<!-- !!! -->
 <?php
 require_once('./checkAdmin.php'); //引入登入判斷
-require_once('../db.inc.php'); //引用資料庫連線
+require_once('./db.inc.php'); //引用資料庫連線
 
 $count = 0;
 $arrParam =[
@@ -12,21 +13,21 @@ for($i = 0; $i < count( $arrParam ); $i++){
    
 
     //找出特定 itemId 的資料
-    $sqlImg = "SELECT `orderId` FROM `orders` WHERE `orderId` = ? ";
-    $stmt_img = $pdo->prepare($sqlImg);
-    $stmt_img->execute($arrParam);
+    // $sqlImg = "SELECT `orderId` FROM `orders` WHERE `orderId` = ? ";
+    // $stmt_img = $pdo->prepare($sqlImg);
+    // $stmt_img->execute($arrParam);
       
-    print_r($arrParam);
+    // print_r($arrParam);
     //有資料，則進行檔案刪除
-    if($stmt_img->rowCount() > 0) {
+    // if($stmt_img->rowCount() > 0) {
         //取得檔案資料 (單筆)
-        $arr = $stmt_img->fetchAll();
+        // $arr = $stmt_img->fetchAll();
         
         //刪除檔案
         // $bool = unlink("../images/items/".$arr[0]['itemImg']);
 
         //若檔案刪除成功，則刪除資料
-        if(isset($arr)){
+        // if(isset($arr)){
             //SQL 語法
             $sql = "DELETE FROM `orders` WHERE `orderId` = ? ";
             $stmt = $pdo->prepare($sql);
@@ -35,18 +36,18 @@ for($i = 0; $i < count( $arrParam ); $i++){
             //累計每次刪除的次數
             $count += $stmt->rowCount();
         };
-    }
-}
+//     }
+// }
 
 if($count > 0) {
-    header("Refresh:3;url=./admin.php");
+    header("Refresh:3;url=../backStage/admin.php");
     $objResponse['success'] = true;
     $objResponse['code'] = 200;
     $objResponse['info'] = "刪除成功";
     echo json_encode($objResponse, JSON_UNESCAPED_UNICODE);
     exit();
 } else {
-    header("Refresh:3;url=./admin.php");
+    header("Refresh:3;url=../backStage/admin.php");
     $objResponse['success'] = false;
     $objResponse['code'] = 500;
     $objResponse['info'] = "刪除失敗";
