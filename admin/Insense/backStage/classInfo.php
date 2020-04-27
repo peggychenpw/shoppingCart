d17f93b11c37c36ae999239d26029dcfc7904e18
YiCheng <dekknm0971@gmail.com>
2020-04-27 18:24:29 +0800

    test

--- admin/Insense/backStage/classInfo.php
+++ admin/Insense/backStage/classInfo.php
@@ -23,20 +23,31 @@ if (isset($_GET['id'])) {
   if ($stmt->rowCount() > 0) {
     $arr = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 ?>
-    <div>
-      <a href="">
-        <div><?php echo $arr['classId'] ?></div>
-      </a>
-    </div>
-    <input type="text" value="<?php echo $arr['classImg'] ?>">
-    <input type="text" value="<?php echo $arr['classPeopleLimit'] ?>">
-    <input type="text" value="<?php echo $arr['classPrice'] ?>">
-    <input type="text" value="<?php echo $arr['classCategoryName'] ?>">
-    <input type="text" value="<?php echo $arr['classDate'] ?>">
-    <input type="text" value="<?php echo $arr['classTime'] ?>">
-    <input type="text" value="<?php echo $arr['shopName'] ?>">
-    <input type="text" value="<?php echo $arr['created_at'] ?>" disabled>
-    <input type="text" value="<?php echo $arr['updated_at'] ?>" disabled>
+    <label for="classId">課程編號：</label>
+    <input type="text" id="classId" value="<?php echo $arr['classId'] ?>">
+    <br>
+    <label for="classImg">課程圖片:</label>
+    <input type="text" id="classImg" value="<?php echo $arr['classImg'] ?>">
+    <br>
+    <label for="classPeopleLimit">人數上限:</label>
+    <input type="text" id="classPeopleLimit" value="<?php echo $arr['classPeopleLimit'] ?>">
+    <br>
+    <label for="classPrice">課程價格:</label>
+    <input type="text" id="classPrice" value="<?php echo $arr['classPrice'] ?>">
+    <br>
+    <label for="classCategoryName">課程類別:</label>
+    <input type="text" id="classCategoryName" value="<?php echo $arr['classCategoryName'] ?>">
+    <br>
+    <label for="classDate">課程日期</label>
+    <input type="text" id="classDate" value="<?php echo $arr['classDate'] ?>">
+    <label for="classTime">課程時間</label>
+    <input type="text" id="classTime" value="<?php echo $arr['classTime'] ?>">
+    <label for="shopName">廠商名稱</label>
+    <input type="text" id="shopName" value="<?php echo $arr['shopName'] ?>">
+    <label for="created_at">新增時間</label>
+    <input type="text" id="created_at" value="<?php echo $arr['created_at'] ?>" disabled>
+    <label for="updated_at">更新時間</label>
+    <input type="text" id="updated_at" value="<?php echo $arr['updated_at'] ?>" disabled>
 
 
 <?php
